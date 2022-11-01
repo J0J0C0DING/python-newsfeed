9d3d83f634a0ffb6963239e2ad69995b6afd5b66
J0J0C0DING <102717784+J0J0C0DING@users.noreply.github.com>
2022-11-01 18:08:36 -0500

    Login/signup routes added, sessions added, templates/routes updated to conditionally render navigation using session data

---
+++ app/routes/api.py
@@ -0,0 +1,65 @@
+from flask import Blueprint, request, jsonify, session
+from app.models import User
+from app.db import get_db
+
+# To see eerror messages
+import sys
+
+bp = Blueprint('api', __name__, url_prefix='/api')
+
+@bp.route('/users', methods=['POST'])
+def signup():
+  data = request.get_json()
+  db = get_db()
+
+  try:
+    # Attempt creating a new user
+    newUser = User(
+      username = data['username'],
+      email = data['email'],
+      password = data['password']
+    )
+    # Save in database
+    db.add(newUser)
+    db.commit()
+
+  except:
+    # Insert failed, send error to front end
+    print(sys.exc_info()[0])
+    db.rollback()
+    return jsonify(message = 'Signup failed'), 500
+
+  # Create/add new user session
+  session.clear()
+  session['user_id'] = newUser.id
+  session['loggedIn'] = True
+
+  return jsonify(id = newUser.id)
+
+@bp.route('/users/logout', methods=['POST'])
+def logout():
+  # remove session variables
+  session.clear()
+  return '', 204
+
+@bp.route('/users/login', methods=['POST'])
+def login():
+  data = request.get_json()
+  db = get_db()
+
+  # db checks if email exists
+  try:
+    user = db.query(User).filter(User.email == data['email']).one()
+  except:
+    print(sys.exc_info()[0])
+
+    return jsonify(message = 'Incorrect login information'), 400
+  
+  if user.verify_password(data['password']) == False:
+    return jsonify(message = 'Incorrect login information'), 400
+  
+  session.clear()
+  session['user_id'] = user.id
+  session['loggedIn'] = True
+
+  return jsonify(id = user.id)
